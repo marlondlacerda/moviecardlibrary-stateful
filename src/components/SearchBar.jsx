181014e9c95313966591ea463804025be10ec55d
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-07-27 19:13:37 -0300

    Enviando o primeiro commit para o repositório do Projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,23 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="searchText">
+            Inclui o texto:
+            <input
+              data-testid="text-input"
+              type="text"
+              id="searchText"
+              name="searchText"
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
