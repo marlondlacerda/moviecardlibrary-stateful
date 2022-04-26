b030ec75a3b53ae4453ee5560ba4d5ee0692ff3f
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-04-26 19:59:28 -0300

    feat: 🎸 add index

---
+++ src/components/AddMovie/AddMovieComponents/index.jsx
@@ -0,0 +1,6 @@
+export { default as Genre } from './Genre';
+export { default as ImagePath } from './ImagePath';
+export { default as InputRating } from './InputRating';
+export { default as Title } from './Title';
+export { default as Subtitle } from './Subtitle';
+export { default as Storyline } from './Storyline';
