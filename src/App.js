142b98816749ddb3cdf1144ea96decf36741ac20
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-07 21:58:02 -0300

    feat: 🎸 add loading and import new changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FeaturedMovie from './components/FeaturedMovie';
 import MovieLibrary from './components/MovieLibrary';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import movies from './data';
+import loading from './images/loading1.gif';
+import loadingMobile from './images/loadingmobile.gif';
 
 import './App.css';
 
+const TWO_SECONDS = 2000;
+
 function App() {
+  const [isCardsLoading, setIsCardsLoading] = useState(true);
+
   const index = Math.floor(Math.random() * movies.length);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsCardsLoading(false);
+    }, TWO_SECONDS);
+  });
 
   return (
     <div className="App">
@@ -16,6 +29,17 @@ function App() {
       <div className="lists">
         <MovieLibrary movies={ movies } />
       </div>
+      <Footer />
+
+      {
+        isCardsLoading
+          && (
+            <div className="loading">
+              <img className="load-desktop" src={ loading } alt="loading" />
+              <img className="load-mobile" src={ loadingMobile } alt="loading" />
+            </div>
+          )
+      }
     </div>
   );
 }
