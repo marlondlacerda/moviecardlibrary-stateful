3e1a4e3ed21ae863154caf3f32136389b6a6f405
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-09 17:38:12 -0300

    refactor: 💡 fix input number

--- src/components/AddMovie/AddMovieComponents/InputRating.jsx
+++ src/components/AddMovie/AddMovieComponents/InputRating.jsx
@@ -60,6 +60,8 @@ class InputRating extends Component {
             placeholder="Digite o nome do Filme!"
             min="0"
             max="10"
+            onKeyDown={ (e) => e.preventDefault() }
+            readOnly
           />
           <span
             role="button"
